1a2dc46e6936f5f8c31ebe921e402293b97d16db
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2023-01-26 14:11:13 +0100

    Update app.py
    Removed run-colab

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import gdown
 app = Flask(__name__)
 
-@app.route('/run-colab')
+@app.route('/')
 def run_colab():
     gdown.download('https://colab.research.google.com/drive/1AnkaT2HS5xfZVcIozbBqqDGSS8gEqOD0', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
