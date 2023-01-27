313b63b2ff377d533678cdeba51bfbccfe15c868
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2023-01-27 13:21:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def run_colab():
+    retun "AI was triggered!"
     gdown.download('https://colab.research.google.com/drive/1AnkaT2HS5xfZVcIozbBqqDGSS8gEqOD0', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
 
