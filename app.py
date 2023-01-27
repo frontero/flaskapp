dafde3d76bcb188923d8bb47f760b27ef7a03207
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2023-01-27 20:31:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import gdown
 
 app = Flask(__name__)
 
-@app.route('/')
-def run_colab():    
+@app.route('/download')
+def download_colab():    
     success = gdown.download('https://colab.research.google.com/drive/1AnkaT2HS5xfZVcIozbBqqDGSS8gEqOD0', 'colab.ipynb', quiet=False)
     if success:
         return jsonify(message='colab notebook downloaded successfully')
     else:
         return jsonify(message='error while downloading colab notebook')
 
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+    app.run()
