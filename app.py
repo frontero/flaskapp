0492eef806e78a128408e29208d74af3c803a3b5
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2023-01-27 15:36:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import gdown
 
 app = Flask(__name__)
 
-@app.route('/run-colab'')
+@app.route('/run-colab')
 def run_colab():
     retun "AI was triggered!"
     gdown.download('https://colab.research.google.com/drive/1AnkaT2HS5xfZVcIozbBqqDGSS8gEqOD0', 'colab.ipynb', quiet=False)
