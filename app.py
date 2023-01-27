4b454029def25655bd000016ed7a1eafbae11cbd
Robert van Dijk <32875079+frontero@users.noreply.github.com>
2023-01-27 15:47:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-return "AI was triggered!"
-
 from flask import Flask, jsonify
 import gdown
 
 app = Flask(__name__)
 
 @app.route('/run-colab')
-def run_colab():
-    return "AI was triggered! 2"
+def run_colab():    
     gdown.download('https://colab.research.google.com/drive/1AnkaT2HS5xfZVcIozbBqqDGSS8gEqOD0', 'colab.ipynb', quiet=False)
-    return jsonify(message='colab notebook ran successfully')
+    #return jsonify(message='colab notebook ran successfully')
+    return "AI was triggered! 2"
 
 if __name__ == '__main__':
     app.run()
